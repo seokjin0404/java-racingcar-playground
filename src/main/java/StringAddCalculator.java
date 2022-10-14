cd02b58901c75ff2fc5e6d4941b0610848d149f4
TSTP <86605720+seokjin0404@users.noreply.github.com>
2022-10-15 00:48:54 +0900

    feat(문자열 계산기) test6 완료

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -1,18 +1,38 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static jdk.nashorn.internal.runtime.JSType.isNumber;
 
 public class StringAddCalculator {
-    public static int splitAndSum(String text) {
+
+    public static int splitAndSum(String text) throws RuntimeException {
         if (text == null || text.isEmpty())
             return 0;
-        String[] numbers = text.split(",|:");
-        return sum(numbers);
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            return sum(getTokens(m));
+        }
+        return sum(text.split(",|:"));
     }
 
-    private static int sum(String[] numbers) {
+    private static String[] getTokens(Matcher m) {
+        String customDelimiter = m.group(1);
+        String[] tokens = m.group(2).split(customDelimiter);
+        return tokens;
+    }
+
+    private static int sum(String[] numbers) throws RuntimeException {
         int sum = 0;
+        int number;
         for (int i = 0; i<numbers.length;i++) {
-            sum += Integer.parseInt(numbers[i]);
+            number = Integer.parseInt(numbers[i]);
+            check(number);
+            sum += number;
         }
         return sum;
+    }
+    private static void check(int number) throws RuntimeException {
+        if (number < 0)
+            throw new RuntimeException();
     }
 }
